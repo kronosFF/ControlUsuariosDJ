fd19d320e52b6ff0ff090110000c6b76b36e25ff
yeison lopez <yeisonlopez20018@gmail.com>
2023-11-17 15:58:38 -0500

    Creacion Usuarios CMD

--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -48,7 +48,9 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'applications.users',
+)
 
 THIRD_PARTY_APPS = ()
 
@@ -104,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
